ec177abae73f2623012e4859990eeb475a3f7272
Slava K <slava.kostromin.2000@mail.ru>
2022-10-25 22:03:39 +0700

    50

---
+++ Z_50/Program.cs
@@ -0,0 +1,48 @@
+﻿/*
+Задача 50. 
+Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, 
+и возвращает значение этого элемента или же указание, что такого элемента нет.
+*/
+
+void InputMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+            matrix[i, j] = new Random().Next(-10, 11);
+    }
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    Console.WriteLine();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+            Console.Write($"{matrix[i, j]} \t");
+    Console.WriteLine();
+    }
+}
+
+Console.Clear();
+Console.Write("Введите кол-во строк: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите кол-во столбцов: ");
+int m = Convert.ToInt32(Console.ReadLine());
+int[,] matrix = new int[n, m];
+InputMatrix(matrix);
+PrintMatrix(matrix);
+Console.Write("Введите номер строки: ");
+int k = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите номер столбца: ");
+int l = Convert.ToInt32(Console.ReadLine());
+if (k > n || l > m)
+{
+    Console.WriteLine("такого элемента нет");
+}
+else
+{
+    Console.WriteLine($"значение элемента {k} строки и {l} столбца равно {matrix[k-1,l-1]}");
+}
+
+
